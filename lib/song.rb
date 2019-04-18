aa0f1990fac80fa24b2e6f1793a11c6c9967c138
James Pier <jpier2012@gmail.com>
2019-04-18 13:52:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,14 @@
 class Song
+  @@artists = []
+  @@genres = []
+  @@count = 0
+  @@artist_count = 0
+  @@genre_count = 0
 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+  end
 end
